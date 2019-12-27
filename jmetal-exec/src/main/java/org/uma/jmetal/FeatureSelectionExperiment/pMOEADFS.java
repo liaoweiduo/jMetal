e206c399497fcfcf592df41ab53ff28d22ca2e45
liaoweiduo <11849249@mail.sustech.edu.cn>
2019-12-27 15:27:31 +0800

    Update pMOEADFS.java

--- jmetal-exec/src/main/java/org/uma/jmetal/FeatureSelectionExperiment/pMOEADFS.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/FeatureSelectionExperiment/pMOEADFS.java
@@ -7,10 +7,7 @@ import org.uma.jmetal.operator.MutationOperator;
 import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
 import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
 import org.uma.jmetal.problem.Problem;
-import org.uma.jmetal.problem.multiobjective.FeatureSelection.Australian;
-import org.uma.jmetal.problem.multiobjective.FeatureSelection.Madelon;
-import org.uma.jmetal.problem.multiobjective.FeatureSelection.Sonar;
-import org.uma.jmetal.problem.multiobjective.FeatureSelection.Vehicle;
+import org.uma.jmetal.problem.multiobjective.FeatureSelection.*;
 import org.uma.jmetal.qualityindicator.impl.GenerationalDistance;
 import org.uma.jmetal.qualityindicator.impl.InvertedGenerationalDistance;
 import org.uma.jmetal.qualityindicator.impl.InvertedGenerationalDistancePlus;
@@ -82,8 +79,8 @@ public class pMOEADFS {
     List<ExperimentProblem<DoubleSolution>> problemList = new ArrayList<>();
 //      problemList.add(new ExperimentProblem<>(new Australian()));
 //    problemList.add(new ExperimentProblem<>(new Vehicle()));  //.changeReferenceFrontTo("DTLZ1.2D.pf")
-      problemList.add(new ExperimentProblem<>(new Sonar()));
-//      problemList.add(new ExperimentProblem<>(new Hillvalley()));
+//      problemList.add(new ExperimentProblem<>(new Sonar()));
+      problemList.add(new ExperimentProblem<>(new Hillvalley()));
 //      problemList.add(new ExperimentProblem<>(new Musk1()));
 //      problemList.add(new ExperimentProblem<>(new Arrhythmia()));
 //      problemList.add(new ExperimentProblem<>(new Madelon()));
